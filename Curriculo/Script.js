44a2a638726871c9507151448970bd4434e34be3
Nicolas Thomas Luizetti <96789314+nicolastluizetti@users.noreply.github.com>
2026-06-20 14:22:19 -0300

    Update Script.js
    adicionado som aos links

--- Curriculo/Script.js
+++ Curriculo/Script.js
@@ -3,3 +3,11 @@ const toggle = document.getElementById("tema-toggle");
 toggle.addEventListener("change", () => {
     document.body.classList.toggle("modo-escuro");
 });
+
+const audioClick = new Audio('./assets/click.mp3');
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        audioClick.currentTime = 0;
+        audioClick.play();
+    });
