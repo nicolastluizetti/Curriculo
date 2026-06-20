4f28a233badcc30cd65fff84bc497fe2981956b2
Nicolas Thomas Luizetti <96789314+nicolastluizetti@users.noreply.github.com>
2026-06-20 14:27:12 -0300

    Update Script.js

--- Curriculo/Script.js
+++ Curriculo/Script.js
@@ -4,10 +4,4 @@ toggle.addEventListener("change", () => {
     document.body.classList.toggle("modo-escuro");
 });
 
-const audioClick = new Audio('./assets/click.mp3');
 
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', () => {
-        audioClick.currentTime = 0;
-        audioClick.play();
-    });
